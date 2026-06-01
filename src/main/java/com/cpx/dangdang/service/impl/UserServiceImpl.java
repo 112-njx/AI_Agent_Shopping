a579dce11fda76822f7eaaf1cb6490c896c7791e
112-njx <1323407968@qq.com>
2026-06-01 10:24:32 +0800

    解决注解报错问题

--- src/main/java/com/cpx/dangdang/service/impl/UserServiceImpl.java
+++ src/main/java/com/cpx/dangdang/service/impl/UserServiceImpl.java
@@ -13,12 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
-
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-    @Resource
+    @Autowired
     private UserDao userDao;
 
     @Transactional(propagation = Propagation.SUPPORTS,readOnly = true)
--- src/test/java/com/cpx/dangdang/test/TestDangDang.java
+++ src/test/java/com/cpx/dangdang/test/TestDangDang.java
@@ -1,7 +1,9 @@
 package com.cpx.dangdang.test;
 
-import com.cpx.dangdang.DangDangApplication;
+import com.cpx.dangdang.dao.UserDao;
 import com.cpx.dangdang.entity.Book;
+import com.cpx.dangdang.entity.Cart;
+import com.cpx.dangdang.entity.CartItem;
 import com.cpx.dangdang.entity.Order;
 import com.cpx.dangdang.entity.User;
 import com.cpx.dangdang.service.*;
@@ -9,9 +11,12 @@ import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Date;
 
@@ -63,19 +68,37 @@ public class TestDangDang {
     @Test
     public void testRegister(){
         User user = new User();
-        user.setEmail("cpx@qq.com");
+        user.setEmail("t" + System.currentTimeMillis() / 1000 + "@q.com");
         user.setCode("122343");
         user.setSalt("1234");
-        user.setNickname("cpx");
+        user.setNickname("testuser");
         user.setCreateDate(new Date());
         user.setStatus(0);
         user.setPassword("123456");
         userService.register(user);
+        System.out.println("注册成功");
     }
+
+    @Autowired
+    private UserDao userDao;
 
     @Test
     public void testValidateEmail(){
-        userService.validateEmail("cpx@qq.com","tl0v6a");
+        // 先注册新用户
+        User user = new User();
+        String email = "v" + System.currentTimeMillis() / 1000 + "@q.com";
+        user.setEmail(email);
+        user.setPassword("123456");
+        user.setSalt("1234");
+        user.setNickname("validatetest");
+        user.setCreateDate(new Date());
+        user.setStatus(0);
+        user.setCode("tmp");
+        userService.register(user);
+        // register() 内部会用随机码覆盖 code，需从数据库读取真实激活码
+        User dbUser = userDao.selectUserByEmail(email);
+        userService.validateEmail(email, dbUser.getCode());
+        System.out.println("激活成功");
     }
 
 
@@ -84,8 +107,25 @@ public class TestDangDang {
 
     @Test
     public void testSaveOrder(){
-        Order order = orderService.saveOrder(1);
+        // 构造 mock HTTP session，因为 saveOrder 依赖从 session 获取购物车和用户
+        MockHttpSession session = new MockHttpSession();
+        User user = new User();
+        user.setUserId(1);
+        user.setEmail("xiaobai@qq.com");
+        session.setAttribute("user", user);
 
+        Cart cart = new Cart();
+        cart.getBuyBooks().put(14, new CartItem(14, "红楼梦", 88.0, 100.0, "hlm.jpg", 1));
+        cart.setTotalMoney(88.0);
+        cart.setSaveMoney(12.0);
+        session.setAttribute("cart", cart);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setSession(session);
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        Order order = orderService.saveOrder(1);
+        System.out.println("order = " + order);
     }
 
     @Test
